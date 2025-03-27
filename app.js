e6ff41f2daab662f9f27b6aca8a899d5d558b69f
Nicolo Veneziale <nicoloveneziale@gmail.com>
2025-03-27 08:43:30 +0000

    Added styling

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 const router = require("./routes/indexRouter");
+const assetsPath = path.join(__dirname, "public");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
 
 const PORT = process.env.PORT || 3000;
 
